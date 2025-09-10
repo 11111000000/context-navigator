715adeecf638d5987b8da9feccb5241e7e8b9871
Peter Kosov <11111000000@email.com>
2025-09-10 14:55:43 +0800

    1.1.7

--- context-navigator.el
+++ context-navigator.el
@@ -5,7 +5,7 @@
 ;; Maintainer: Peter Kosov <11111000000@email.com>
 ;; URL: https://github.com/11111000000/context-navigator
 ;; Keywords: convenience, tools
-;; Version: 1.1.6
+;; Version: 1.1.7
 ;; Package-Requires: ((emacs "29.1") (transient "0.3.0"))
 
 ;;; Commentary:
@@ -66,8 +66,8 @@
 (defun context-navigator-version ()
   "Return version string for Context Navigator."
   (interactive)
-  (message "context-navigator — 1.1.3")
-  "1.1.3")
+  (message "context-navigator — 1.1.7")
+  "1.1.7")
 
 ;;;###autoload
 (defun context-navigator-start ()
